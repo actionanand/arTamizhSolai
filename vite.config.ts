6a5c356f5fd07cf935e7db9426383f8dedf1ae85
Anand raja <46064269+actionanand@users.noreply.github.com>
2025-12-15 13:16:22 +0000

    vite config change

--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,34 @@ import { defineConfig } from 'vite';
 import analog from '@analogjs/platform';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  build: {
-    target: ['es2020'],
-  },
-  resolve: {
-    mainFields: ['module'],
-  },
-  plugins: [
-    analog({
-      content: {
-        highlighter: 'shiki',
-      },
-      prerender: {
-        routes: ['/blog', '/blog/2022-12-27-my-first-post'],
-      },
-    }),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/test-setup.ts'],
-    include: ['**/*.spec.ts'],
-    reporters: ['default'],
-  },
-}));
+export default defineConfig(({ mode }) => {
+  const isGitHubPages = process.env['GITHUB_PAGES'] === 'true';
+  const base = isGitHubPages ? '/artamizhsolai/' : '/';
+
+  return {
+    base,
+    build: {
+      target: ['es2020'],
+    },
+    resolve: {
+      mainFields: ['module'],
+    },
+    plugins: [
+      analog({
+        content: {
+          highlighter: 'shiki',
+        },
+        prerender: {
+          routes: ['/blog', '/blog/2022-12-27-my-first-post'],
+        },
+      }),
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/test-setup.ts'],
+      include: ['**/*.spec.ts'],
+      reporters: ['default'],
+    },
+  };
+});
